b3d83bfd92ea630f46745389af3a0212f4ca8b80
joe95-ux <ogorktabi@gmail.com>
2025-03-11 23:39:41 +0200

    add deployed contract address

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import db from "@/lib/db";
+import { db } from "@/lib/db";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Activity,
--- lib/db.ts
+++ lib/db.ts
@@ -8,8 +8,7 @@ declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-const db = globalThis.prismaGlobal ?? prismaClientSingleton();
+export const db = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default db;
 
 if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db;
