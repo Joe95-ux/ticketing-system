3a62c302c3570b3a09b0f5aa43cedeae45d51de7
joe95-ux <ogorktabi@gmail.com>
2025-03-28 12:08:18 +0200

    fix node modules bug

--- next.config.ts
+++ next.config.ts
@@ -12,14 +12,6 @@ const nextConfig: NextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      react: require.resolve('react'),
-      'react-dom': require.resolve('react-dom')
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
