8770ec7ebdaf25498f365698d6cf60c8418aef8d
joe95-ux <ogorktabi@gmail.com>
2025-03-28 02:43:05 +0200

    downgrade react version

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,14 @@ const nextConfig: NextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: require.resolve('react'),
+      'react-dom': require.resolve('react-dom')
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
