411563a7811786972baa3689e92f9934beca713c
joe95-ux <ogorktabi@gmail.com>
2025-07-09 22:52:08 +0200

    fix activity log model

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,15 +1,22 @@
 import "next-auth";
+import "next-auth/jwt";
 import { Role } from "@prisma/client";
 
 declare module "next-auth" {
   interface User {
     role: Role;
   }
-  
+
   interface Session {
     user: User & {
       id: string;
-      role: Role;
     };
   }
-} 
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    role: Role;
+  }
+}
